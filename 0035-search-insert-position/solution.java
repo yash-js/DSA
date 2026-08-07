8e2b58a3cda4e9e4c0d26c0e915dad594058f716
Yash Purani <31120678+yash-js@users.noreply.github.com>
2026-08-08 00:32:47 +0530

    Update: 0035. Search Insert Position (Java) — code

--- 0035-search-insert-position/solution.java
+++ 0035-search-insert-position/solution.java
@@ -1,15 +1,23 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        for(int i=0; i<nums.length;i++){
-            if(nums[i] == target){
+        // for(int i=0; i<nums.length;i++){
+        //     if(nums[i] == target){
+        //         return i;
+        //     } else if(target <= nums[i]){
+        //         return i;
+        //     } else if(i == nums.length -1){
+        //         return i+1;
+        //     }
+        // }
+
+        // return 0;
+
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
                 return i;
-            } else if(target <= nums[i]){
-                return i;
-            } else if(i == nums.length -1){
-                return i+1;
             }
         }
-
-        return 0;
+        return nums.length;
     }
 }
